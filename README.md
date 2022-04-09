0563e2a43d4442d4937c6400f87491fdc5f4ab82
Grajal <jorgegrajal@gmail.com>
2022-04-10 00:31:32 +0200

    Array ordenado representado

---
+++ README.md
@@ -0,0 +1,3 @@
+PRIMER PROGRAMA USANDO HTML, CSS Y JS
+
+El programa consiste en ordenar unos numeros aleatorios.
---
+++ codigo.js
@@ -0,0 +1,88 @@
+function numeroAleatorio(){
+
+    return Math.floor(Math.random() * 10 + 1);
+
+}
+
+function crearDivs(array, tam, padre){
+
+    for(let i = 0; i < tam; i++){
+
+        const itemDiv = document.createElement("div");
+
+        itemDiv.textContent = array[i];
+
+        padre.appendChild(itemDiv);
+
+        itemDiv.className = "subDiv";
+
+    }
+
+}
+
+do{
+
+    var tam = prompt("Cuantos numeros quieres ordenar?: ");
+
+    if(tam > 17 || tam < 2){
+
+        alert("El numero de elementos a ordenar tiene que estar comprendido entre 2 y 17");
+
+    }
+
+}while(tam > 17 || tam < 2);
+
+let array = new Array(tam);
+
+for(let i = 0; i < tam; i++){
+
+    array[i] = numeroAleatorio();
+
+}
+
+const divPrincipal = document.getElementById("principal");
+
+crearDivs(array, tam, divPrincipal);
+
+let arrayDivs = document.querySelectorAll(".subDiv");
+
+console.log(arrayDivs)
+
+let aux;
+
+array = Array.apply(null, arrayDivs);
+
+let arrayNums = new Array(array.length);
+
+for(let i = 0; i < array.length; i++){
+
+    let numberTxt = arrayDivs[i].textContent;
+
+    arrayNums[i] = parseInt(numberTxt);
+
+}
+
+for(let i = 0; i < array.length; i++){
+    for(let j = 0; j < array.length; j++){
+
+        if(arrayNums[j] > arrayNums[j + 1]){
+
+            aux = arrayNums[j];
+            arrayNums[j] = arrayNums[j + 1];
+            arrayNums[j + 1] = aux;
+
+        }
+
+    }
+}
+
+console.log(arrayNums);
+
+const divOrdenado = document.createElement("div");
+const body = document.getElementById("body");
+
+body.appendChild(divOrdenado);
+console.log(divOrdenado);
+
+crearDivs(arrayNums, tam, divOrdenado);
+
